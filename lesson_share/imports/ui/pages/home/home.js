a7577c359e0637f9ab1a8a30071519d402db9c26
Zac Bolton <zacbolton@Zacs-MacBook-Pro.local>
2018-03-20 22:04:02 -0300

    adding file structure p.1

---
+++ lesson_share/imports/ui/pages/home/home.js
@@ -0,0 +1,7 @@
+import {Template} from 'meteor/templating';
+import {Meteor} from 'meteor/meteor';
+
+import './home.html';
+
+import '../../components/banner/banner.js';
+import '../../components/search_page/search_page.js';
