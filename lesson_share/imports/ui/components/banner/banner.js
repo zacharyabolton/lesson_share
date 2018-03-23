fbd7d59abbe59438c4a6bb8affed6e104562801b
Zac Bolton <zacbolton@Zacs-MacBook-Pro.local>
2018-03-22 21:59:41 -0300

    loginButtons added and began

--- lesson_share/imports/ui/components/banner/banner.js
+++ lesson_share/imports/ui/components/banner/banner.js
@@ -2,3 +2,34 @@ import {Template} from 'meteor/templating';
 import {Meteor} from 'meteor/meteor';
 
 import './banner.html';
+
+
+Template.register.events({
+    'submit form': function(event) {
+        event.preventDefault();
+        var emailVar = event.target.registerEmail.value;
+        var passwordVar = event.target.registerPassword.value;
+        console.log("Form submitted.");
+        Accounts.createUser({
+            email: emailVar,
+            password: passwordVar
+        });
+    }
+});
+
+Template.login.events({
+    'submit form': function(event) {
+        event.preventDefault();
+        var emailVar = event.target.loginEmail.value;
+        var passwordVar = event.target.loginPassword.value;
+        console.log("Form submitted.");
+        Meteor.loginWithPassword(emailVar, passwordVar);
+    }
+});
+
+Template.dashboard.events({
+    'click .logout': function(event){
+        event.preventDefault();
+        Meteor.logout();
+    }
+});
